b29de39f7f18107b3cf301797c392ea9d43a592a
brenth <brenth@lumapictures.com>
2024-04-13 10:00:07 +1000

    setting for my keymap

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -4,7 +4,9 @@
 
 DEFAULT_FOLDER = crkbd/rev1
 
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
 
 VIA_ENABLE = yes
+WPM_ENABLE = yes  # Enable word per minute counter
+LTO_ENABLE = yes  # Makes hex file smaller
